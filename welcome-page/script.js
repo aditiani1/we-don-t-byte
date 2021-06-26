5d9e6f144c619e4e311183d6d2c5695da97b1cff
Jai-10 <jai.vaidya18@gmail.com>
2021-06-26 16:05:31 +0530

    added welcome page

---
+++ welcome-page/script.js
@@ -0,0 +1,9 @@
+function change() {
+    
+    $("#login-container").css({
+        "transform": "scale(2.8)"
+    });
+    $("#login-container").empty();
+    window.location.href = "../homepage/index.html";
+ 
+}
